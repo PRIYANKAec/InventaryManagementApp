e4c32141bd1263ee3b121cd25202ac2a4b128587
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-17 10:07:01 +0530

    Implemented Navigation Bar[ADD]

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
+import Dashboard from "@/app/dashboard/page";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <Dashboard />
   );
 }
