ecaa242e0741f475dd719d02649e4228ce52a43d
PRIYANKAec <priyankaa261103@gmail.com>
2024-09-18 10:51:16 +0530

    Implemented Sidebar[ADD]

--- client/src/dashboardwrapper.tsx
+++ client/src/dashboardwrapper.tsx
@@ -20,12 +20,12 @@ const  DashboardLayout =  ({children}: {children: React.ReactNode}) => {
       document.documentElement.classList.add("light")
     }
   })
-  
+
   return (
     <div className={`${ isDarkMode? "dark" : "light"}
     flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar />
-      <main className={`flex flex-col w-full h-full py-2 px-9 bg-gray-50 
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 
       ${isSidebarCollapsed? "md:pl-24": "md-pl-72"} `}>
          <Navbar />
       {children}
